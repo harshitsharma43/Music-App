ddb53fdc82d337b44729c0c9c69cd425e5ff194c
Harshit Sharma <harshitsharma937@gmail.com>
2024-08-30 06:50:41 +0530

    chnages

---
+++ remix.config.ts
@@ -0,0 +1,11 @@
+
+
+const config = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/server.js", // Output directory for the server
+  devServerPort: 8002,
+};
+
+export default config;
